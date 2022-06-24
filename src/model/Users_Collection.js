93324550bcc25b2e51da9cf4ffee85ed56e0e1f4
Sichkar Yurii <si4kar2017@gmail.com>
2022-06-24 20:07:40 +0300

    Lesson_25

--- src/model/Users_Collection.js
+++ src/model/Users_Collection.js
@@ -1,12 +1,12 @@
-const USERS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/users/';
-
 export default class Users_Collection {
+    static USERS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/users/';
+
     constructor() {
         this.list = [];
     }
 
     getUsersList() {
-        return fetch(USERS_URL)
+        return fetch(Users_Collection.USERS_URL)
             .then((res) => res.json())
             .then((data) => (this.list=data));
     }
@@ -14,13 +14,13 @@ export default class Users_Collection {
     removeUsers(todoId) {
         this.list = this.list.filter(({ id }) => id != todoId);
 
-        return fetch(USERS_URL + todoId, {
+        return fetch(Users_Collection.USERS_URL + todoId, {
             method: 'DELETE',
         });
     }
 
     addUsers(user) {
-        return fetch(USERS_URL, {
+        return fetch(Users_Collection.USERS_URL, {
             method: 'POST',
             body: JSON.stringify(user),
             headers: {
@@ -28,7 +28,6 @@ export default class Users_Collection {
             },
         })
             .then((res) => res.json())
-            .then((data) => this.list.push(data));
-        
+            .then((data) => this.list.push(data));        
     }
 }
